b623d7e1ccefd5a99ce7334f67b51cec0ee5aaad
Michael OHare <mjohare13@gmail.com>
2023-07-24 21:37:17 -0400

    Added symbols for pieces, background colors for squares, highlight available moves, captured pieces display, and refactored updateButton method

--- ChessGame/src/main/java/com/michaeljohare/chess/GUI/ChessGUI.java
+++ ChessGame/src/main/java/com/michaeljohare/chess/GUI/ChessGUI.java
@@ -3,9 +3,13 @@ package com.michaeljohare.chess.GUI;
 import com.michaeljohare.chess.game.Player;
 import com.michaeljohare.chess.game.Square;
 import com.michaeljohare.chess.pieces.*;
+import com.sun.jdi.BooleanType;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.michaeljohare.chess.game.Board.*;
@@ -16,14 +20,15 @@ public class ChessGUI extends JFrame {
     /*TO DO:
     * Implement pawn promote
     * Implement undo
-    * Implement display (available moves, captured pieces)
-    * Highlight squares available instead of sout
-    * Choosing square not in move list should allow reselect of different piece
+    * Implement check/checkmate
+    * Implement message display
     * A-H 1-8 Legend
     * */
 
-    private JTextField display;
     private JButton[][] chessButtons;
+    private JTextArea logTextArea;
+    private JTextArea player1CapturedArea;
+    private JTextArea player2CapturedArea;
     private int turnCounter;
     private boolean hasCastled = false;
     private final Player player1 = new Player(PLAYER_1);
@@ -31,6 +36,9 @@ public class ChessGUI extends JFrame {
     private Square sourceSquare;
     private boolean isFirstClick = true;
     private ChessPiece capturedPiece;
+    private List<ChessPiece> player1CapturedPieces = new ArrayList<>();
+    private List<ChessPiece> player2CapturedPieces = new ArrayList<>();
+    private List<Square> highlightedSquares = new ArrayList<>();
     private ChessPiece playerPiece;
     private ChessPiece previousPiece;
 
@@ -40,79 +48,234 @@ public class ChessGUI extends JFrame {
 
     private void initializeGUI() {
         JFrame frame = new JFrame("Chess");
-        frame.setSize(600, 600);
-        frame.setLayout(new GridLayout(8, 8));
+        frame.setSize(1100, 1000);
+        frame.setLayout(new BorderLayout());
 
+        JPanel chessboardPanel = new JPanel(new GridLayout(8, 8));
         chessButtons = new JButton[8][8];
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 chessButtons[row][col] = new JButton();
+                chessButtons[row][col].setFont(new Font("Roboto", Font.PLAIN, 40));
+                if (row % 2 == 0) {
+                    if (col % 2 == 0) {
+                        chessButtons[row][col].setBackground(new Color(248, 240, 198));
+                    } else {
+                        chessButtons[row][col].setBackground(new Color(156, 98, 69));
+                    }
+                } else {
+                    if (col % 2 == 1) {
+                        chessButtons[row][col].setBackground(new Color(248, 240, 198));
+                    } else {
+                        chessButtons[row][col].setBackground(new Color(156, 98, 69));
+                    }
+                }
                 updateButton(row, col);
                 final int finalRow = row;
                 final int finalCol = col;
                 chessButtons[row][col].addActionListener(e -> onSquareClick(finalRow, finalCol));
-                frame.add(chessButtons[row][col]);
+                chessboardPanel.add(chessButtons[row][col]);
             }
         }
+
+        logTextArea = new JTextArea(5, 20);
+        logTextArea.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(logTextArea);
+
+        player1CapturedArea = new JTextArea(15, 8);
+        player2CapturedArea = new JTextArea(15, 8);
+        JLabel capturedPieces1Title = new JLabel("Captured Pieces");
+        capturedPieces1Title.setFont(new Font("Roboto", Font.BOLD, 24));
+        JLabel capturedPieces2Title = new JLabel("Captured Pieces");
+        capturedPieces2Title.setFont(new Font("Roboto", Font.BOLD, 24));
+        player1CapturedArea.add(capturedPieces1Title);
+        player2CapturedArea.add(capturedPieces2Title);
+        player1CapturedArea.setEditable(false);
+        player2CapturedArea.setEditable(false);
+        player1CapturedArea.setLayout(new FlowLayout());
+        player2CapturedArea.setLayout(new FlowLayout());
+        player1CapturedArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        player2CapturedArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.add(player1CapturedArea, BorderLayout.SOUTH);
+        rightPanel.add(logScrollPane, BorderLayout.CENTER);
+        rightPanel.add(player2CapturedArea, BorderLayout.NORTH);
+
+        frame.add(chessboardPanel, BorderLayout.CENTER);
+        frame.add(rightPanel, BorderLayout.EAST);
+
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
         updateGUI();
     }
 
     private void updateButton(int row, int col) {
+        String pieceType = getPieceType(board[row][col]);
+        String player = getPlayer(board[row][col]);
+
+        if (!pieceType.equals(EMPTY)) {
+            String imagePath = getImagePath(pieceType, player);
+            try {
+                Image pieceImage = ImageIO.read(ChessGUI.class.getResource(imagePath));
+                chessButtons[row][col].setIcon(new ImageIcon(pieceImage));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            chessButtons[row][col].setIcon(null);
+        }
+    }
+
+    private String getPieceType(String square) {
+        if (square.startsWith(PAWN)) {
+            return "Pawn";
+        } else if (square.startsWith(ROOK)) {
+            return "Rook";
+        } else if (square.startsWith(BISHOP)) {
+            return "Bishop";
+        } else if (square.startsWith(KNIGHT)) {
+            return "Knight";
+        } else if (square.startsWith(KING)) {
+            return "King";
+        } else if (square.startsWith(QUEEN)) {
+            return "Queen";
+        } else {
+            return EMPTY;
+        }
+    }
+
+    private String getPlayer(String square) {
+        if (square.endsWith(PLAYER_1)) {
+            return PLAYER_1;
+        } else if (square.endsWith(PLAYER_2)) {
+            return PLAYER_2;
+        } else {
+            return EMPTY;
+        }
+    }
+
+    private String getImagePath(String pieceType, String player) {
+        return "/" + (player.equals(PLAYER_1) ? "White_" : "Black_") + pieceType + ".png";
+    }
+
+/*
+    private void updateButton(int row, int col) {
 
         if (board[row][col].startsWith(PAWN)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_PAWN);
+                try {
+                    Image whitePawn = ImageIO.read(ChessGUI.class.getResource("/White_Pawn.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whitePawn));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_PAWN);
+                try {
+                    Image blackPawn = ImageIO.read(ChessGUI.class.getResource("/Black_Pawn.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackPawn));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].startsWith(ROOK)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_ROOK);
+                try {
+                    Image whiteRook = ImageIO.read(ChessGUI.class.getResource("/White_Rook.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whiteRook));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_ROOK);
+                try {
+                    Image blackRook = ImageIO.read(ChessGUI.class.getResource("/Black_Rook.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackRook));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].startsWith(BISHOP)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_BISHOP);
+                try {
+                    Image whiteBishop = ImageIO.read(ChessGUI.class.getResource("/White_Bishop.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whiteBishop));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_BISHOP);
+                try {
+                    Image blackBishop = ImageIO.read(ChessGUI.class.getResource("/Black_Bishop.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackBishop));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].startsWith(KNIGHT)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_KNIGHT);
+                try {
+                    Image whiteKnight = ImageIO.read(ChessGUI.class.getResource("/White_Knight.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whiteKnight));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_KNIGHT);
+                try {
+                    Image blackKnight = ImageIO.read(ChessGUI.class.getResource("/Black_Knight.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackKnight));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].startsWith(KING)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_KING);
+                try {
+                    Image whiteKing = ImageIO.read(ChessGUI.class.getResource("/White_King.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whiteKing));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_KING);
+                try {
+                    Image blackKing = ImageIO.read(ChessGUI.class.getResource("/Black_King.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackKing));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].startsWith(QUEEN)) {
             if (board[row][col].endsWith(PLAYER_1)) {
-                chessButtons[row][col].setText(WHITE_QUEEN);
+                try {
+                    Image whiteQueen = ImageIO.read(ChessGUI.class.getResource("/White_Queen.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(whiteQueen));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             } else {
-                chessButtons[row][col].setText(BLACK_QUEEN);
+                try {
+                    Image blackQueen = ImageIO.read(ChessGUI.class.getResource("/Black_Queen.png"));
+                    chessButtons[row][col].setIcon(new ImageIcon(blackQueen));
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
             }
         }
 
         if (board[row][col].equals(EMPTY)) {
-            chessButtons[row][col].setText(EMPTY);
+            chessButtons[row][col].setIcon(null);
         }
     }
+*/
 
     private void onSquareClick(int row, int col) {
         if (isFirstClick) {
@@ -132,9 +295,9 @@ public class ChessGUI extends JFrame {
                     playerPiece = null;
                     isFirstClick = true;
                 }else {
-                    System.out.println("\nThese are the available moves");
                     for (Square square : moves) {
-                        System.out.println(square);
+                        chessButtons[square.getX()][square.getY()].setBorder(BorderFactory.createLineBorder(Color.YELLOW, 4));
+                        highlightedSquares.add(square);
                     }
                     isFirstClick = false;
                 }
@@ -147,6 +310,8 @@ public class ChessGUI extends JFrame {
                     if (turnCounter % 2 == 0) {
                         capturedPiece = player2.getPlayerPiece(new Square(row, col));
                         player2.capturePiece(capturedPiece);
+                        player1CapturedPieces.add(capturedPiece);
+                        updateCapturedPiecesDisplay();
                         hasCastled = false;
 /*                        if (playerPiece instanceof Pawn && playerPiece.getCurrentSquare().getX() == 0) {
                             System.out.print("What do you want to promote to? Enter corresponding number eg. 1 " +
@@ -161,6 +326,8 @@ public class ChessGUI extends JFrame {
                     } else {
                         capturedPiece = player1.getPlayerPiece(new Square(row, col));
                         player1.capturePiece(capturedPiece);
+                        player2CapturedPieces.add(capturedPiece);
+                        updateCapturedPiecesDisplay();
                         hasCastled = false;
 /*                        if (playerPiece instanceof Pawn && playerPiece.getCurrentSquare().getX() == 7) {
                             System.out.print("What do you want to promote to? Enter corresponding number eg. 1 " +
@@ -223,11 +390,18 @@ public class ChessGUI extends JFrame {
                     }*/
                 }
                 updateGUI();
+                clearHighlightedSquares();
                 previousPiece = playerPiece;
                 playerPiece = null;
                 sourceSquare = null;
                 isFirstClick = true;
                 turnCounter++;
+            } else {
+                System.out.println("The square you chose to move to is not a legal move, choose a piece and try again.");
+                clearHighlightedSquares();
+                playerPiece = null;
+                sourceSquare = null;
+                isFirstClick = true;
             }
         }
         // UNDO logic
@@ -275,6 +449,7 @@ public class ChessGUI extends JFrame {
 
     }
 
+
     public void updateGUI() {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
@@ -283,8 +458,36 @@ public class ChessGUI extends JFrame {
         }
     }
 
+    private void updateCapturedPiecesDisplay() {
+        player1CapturedArea.removeAll();
+        player2CapturedArea.removeAll();
+
+        for (ChessPiece piece : player1CapturedPieces) {
+            JLabel blackCapturedPieceLabel = new JLabel(piece.getBlackChessPieceSymbol());
+            blackCapturedPieceLabel.setFont(new Font("Roboto", Font.PLAIN, 26));
+            player1CapturedArea.add(blackCapturedPieceLabel);
+        }
+
+        for (ChessPiece piece : player2CapturedPieces) {
+            JLabel whiteCapturedPieceLabel = new JLabel(piece.getWhiteChessPieceSymbol());
+            whiteCapturedPieceLabel.setFont(new Font("Roboto", Font.PLAIN, 26));
+            player2CapturedArea.add(whiteCapturedPieceLabel);
+        }
+
+        player1CapturedArea.revalidate();
+        player1CapturedArea.repaint();
+        player2CapturedArea.revalidate();
+        player2CapturedArea.repaint();
+    }
+
     private boolean isEmpty(int x, int y) {
         return board[x][y].equals(EMPTY);
     }
 
+    private void clearHighlightedSquares() {
+        for (Square square : highlightedSquares) {
+            chessButtons[square.getX()][square.getY()].setBorder(null);
+        }
+        highlightedSquares.clear();
+    }
 }
